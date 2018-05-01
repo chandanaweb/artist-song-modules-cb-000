658c73e4c8ab342838add7e0188387f0097f89a1
chandana M <chandu11288@gmail.com>
2018-05-01 13:37:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  include paramable
+  include Paramable
   extend Findable
   attr_accessor :name
   attr_reader :songs
